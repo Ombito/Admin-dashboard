65a5c64f0d2ba7fddedf14f766650b86b1d2a391
Ombito <alvinombito09@gmail.com>
2024-04-01 00:35:01 +0300

    Fix: home

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Barchart from '../Barchart/barchart';
+import Piechart from '../Piechart/piechart';
+import Linegraph from '../Linegraph/linegraph';
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      {/* <Barchart /> */}
+      {/* <Piechart /> */}
+      <Linegraph />
     </div>
   )
 }
---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2020,
+        worth: 50000,
+    },
+    {
+        id: 2,
+        year: 2021,
+        worth: 90000,
+    },
+    {
+        id: 3,
+        year: 2022,
+        worth: 30000,
+    },
+]
