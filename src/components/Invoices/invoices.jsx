fc337e1fb7f263f7568a876698226a68e273b175
Alvin Ombito <alvinombito09@gmail.com>
2024-10-18 01:58:41 +0300

    Fix: invoices

--- src/components/Invoices/invoices.jsx
+++ src/components/Invoices/invoices.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './invoices.css';
+import user from "../../Assets/user.jpg";
 
 
 const Invoices = () => {
   return (
-    <div>invoices</div>
+    <div className='invoice-hero-container'>
+      <div className="navbar-div">
+        <h2>Invoices</h2>
+        <div className='sidebar-username'>
+          <div className="admin-profile">
+            <img src={user} alt="avatar" />
+          </div>
+        </div>
+      </div>
+        <div className='invoice-hero'></div>
+    </div>
   )
 }
 
