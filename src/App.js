ac1d299ffff3aa601ee07ba30e39527e5ec4324e
Alvin Ombito <alvinombito09@gmail.com>
2024-10-17 23:27:51 +0300

    Fix: add routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Orders from '../src/components/Orders/orders';
 import Users from '../src/components/Users/users';
 import Discounts from '../src/components/Discounts/discounts';
 import Giftcard from '../src/components/Giftcard/giftcard';
+import Messages from '../src/components/Messages/messages';
+import Invoices from '../src/components/Invoices/invoices';
 import Settings from '../src/components/Settings/settings';
 import SignIn from '../src/components/Signin/signin';
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
@@ -33,8 +35,10 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
               <Route path="/orders" element={<Orders />} />
+              <Route path="/messages" element={<Messages />} />
               <Route path="/customers" element={<Users />} />
               <Route path="/discounts" element={<Discounts />} />
+              <Route path="/invoices" element={<Invoices />} />
               <Route path="/gift&vouchers" element={<Giftcard />} />
               <Route path="/settings" element={<Settings />} />
               <Route path="/signin" element={<SignIn setUser={setUser}/>} />
---
+++ src/components/Invoices/invoices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './invoices.css';
+
+
+const Invoices = () => {
+  return (
+    <div>invoices</div>
+  )
+}
+
+export default Invoices;
