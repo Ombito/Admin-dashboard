574985b21781af75ec3c012d374ce4b69c089ac7
Alvin Ombito <alvinombito09@gmail.com>
2024-10-24 18:02:44 +0300

    Fix: user details

--- src/components/UserDetails/userDetails.jsx
+++ src/components/UserDetails/userDetails.jsx
@@ -1,21 +1,30 @@
-// UserDetails.js
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import './userDetails.css';
+import user from "../../Assets/user.jpg";
 
 const UserDetails = () => {
   const location = useLocation();
   const { user } = location.state;
 
   return (
-    <div>
-      <h2>User Details</h2>
+    <div className="userDetails-container">
+        <div className="navbar-div">
+            <div className="navbar-div-hero-section">
+                <h2>User Details</h2>
+            </div>
+            <div className='sidebar-username'>
+                <div className="admin-profile">
+                    <img src={user} alt="avatar" />
+                </div>
+            </div>
+        </div>
       {user ? (
-        <>
+        <div className="userDetails-hero-container">
           <p><strong>Name:</strong> {user.first_name} {user.last_name}</p>
           <p><strong>Email:</strong> {user.email}</p>
           <p><strong>Phone Number:</strong> {user.phone_number}</p>
-          {/* Add more fields as needed */}
-        </>
+        </div>
       ) : (
         <p>No user data available.</p>
       )}
